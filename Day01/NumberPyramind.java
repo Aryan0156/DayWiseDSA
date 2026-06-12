6210d01e6b47032f23a506eb57ecf6bf3cae0e3f
Sahil Khan <arya89nkhan@gmail.com>
2026-06-13 01:25:56 +0530

    Number Pyramid

--- Day01/NumberPyramind.java
+++ Day01/NumberPyramind.java
@@ -1,22 +1,29 @@
-// package Day01;
-
+package Day01;
 import java.util.Scanner;
 
 public class NumberPyramind {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i =0; i<n; i++){
-        for(int k =5; k>i; k--){
-            System.out.print(" ");
-        }
-        for(int j =0; j<=i; j++){
-            System.out.print("* ");
-        }
-        System.out.println();
 
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+
+            for (int k = i - 1; k >= 1; k--) {
+                System.out.print(k+" ");
+            }
+
+            System.out.println();
         }
+
         sc.close();
     }
 }
-    

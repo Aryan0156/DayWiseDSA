dbb8bb8a6792ce4039d17db7ed0a24db84ab9744
Sahil Khan <arya89nkhan@gmail.com>
2026-06-13 13:05:25 +0530

    inversion Number

---
+++ Day01/InversionNumber.java
@@ -0,0 +1,24 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class InversionNumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt(); 
+        for(int i =n; i>0; i--){
+            for(int j = i; j<n; j++){
+                System.out.print("  ");
+            }
+            for(int k =1; k<=i; k++){
+                System.out.print(k+" ");
+            }
+            for(int j =i-1; j>=1; j--){
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+        sc.close();
+    }
+}

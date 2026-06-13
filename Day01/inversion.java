3487114e0909c9752d9d4ac1da5f3b969ae328e5
Sahil Khan <arya89nkhan@gmail.com>
2026-06-13 12:41:38 +0530

    inverse

---
+++ Day01/inversion.java
@@ -0,0 +1,23 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class inversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n; i>0; i--){
+            for(int j = i; j<n; j++){
+                System.out.print(" ");
+            }
+            for(int k =1; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+        sc.close();
+        
+    }
+    
+}

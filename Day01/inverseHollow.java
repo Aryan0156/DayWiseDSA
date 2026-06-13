eee8cc7b6ed66204bff9b1e92f73d7f79ee78439
Sahil Khan <arya89nkhan@gmail.com>
2026-06-13 14:49:47 +0530

    inverseHollow

---
+++ Day01/inverseHollow.java
@@ -0,0 +1,31 @@
+package Day01;
+import java.util.Scanner;
+
+public class inverseHollow {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+        
+            for (int s = n; s > i; s--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+
+                if (i == n || i == 1 || j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}

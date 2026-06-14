dad4cf5b54a21dc8bf4593986d86d82d1c44a1df
Sahil Khan <arya89nkhan@gmail.com>
2026-06-14 13:46:56 +0530

    Right

--- Day01/continuepyramid.java
+++ Day01/continuepyramid.java
@@ -4,7 +4,7 @@ public class continuepyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = 3;
+        int n = sc.nextInt();
 
         for (int b = 0; b < n; b++) {
 

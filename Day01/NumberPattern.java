0be1def7b1dd4ae51d8b94ef425578c263006534
Sahil Khan <arya89nkhan@gmail.com>
2026-06-14 12:26:09 +0530

    Number pattern18

---
+++ Day01/NumberPattern.java
@@ -0,0 +1,27 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int size = 2*n-1;
+        for(int i=0; i<size; i++){
+            for(int j =0; j<size; j++){
+                int top=i;
+                int bottom = size-1-i;
+                int left =j;
+                int right = size -1-j;
+                int temp = Math.min(Math.min(top,bottom), Math.min(left,right));
+                System.out.print(n-temp+" ");
+
+
+            }
+            System.out.println();
+        }
+        sc.close();
+
+
+    }
+}

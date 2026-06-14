fbd436002610940794e742f7d1b612b5ac3daf0b
Sahil Khan <arya89nkhan@gmail.com>
2026-06-14 13:37:30 +0530

    conitnuepyramid

--- Day01/continuepyramid.java
+++ Day01/continuepyramid.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-public class continuePyramid {
+public class continuepyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int n = 3;
 
         for (int b = 0; b < n; b++) {
 
             for (int i = 0; i < n; i++) {
 
                 
-                for (int j = 0; j < b * 4; j++) {
+                for (int j = 0; j <= b * (n+2); j++) {
                     System.out.print(" ");
                 }
 
                 
                 for (int k = 0; k <= i; k++) {
-                    System.out.print("* ");
+                    System.out.print("*");
                 }
 
                 System.out.println();

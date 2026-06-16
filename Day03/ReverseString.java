57be21c45a8862c2852dd995ae10b16f2a84aaf1
Sahil Khan <arya89nkhan@gmail.com>
2026-06-16 16:42:59 +0530

    Reverse

---
+++ Day03/ReverseString.java
@@ -0,0 +1,28 @@
+package Day03;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    static void reverse(char [] arr,int left, int right) {
+        if(left >= right){
+            return;
+        }
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        reverse(arr, left+1, right-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        char [] arr = str.toCharArray();
+        reverse(arr, 0, arr.length-1);
+
+        System.out.println(new String(arr));
+    }
+}

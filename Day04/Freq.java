22b44cb1037383909f73da5e4edd09718d5f875d
Sahil Khan <arya89nkhan@gmail.com>
2026-06-17 23:39:27 +0530

    update

---
+++ Day04/Freq.java
@@ -0,0 +1,31 @@
+package Day04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Freq {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int count = 1;
+        for(int i=1; i<n; i++){
+            if(arr[i] == arr[i-1]){
+                count++;
+            }
+            else{
+                System.out.println(arr[i-1]+" -> " +count);
+                count=1;
+            }
+            
+        }
+        System.out.println(arr[n-1]+" -> " +count);
+        
+        sc.close();
+    }
+    
+}
---
+++ Day05/Consecutiveone.java
@@ -0,0 +1,8 @@
+package Day05;
+
+public class Consecutiveone {
+    public static void main(String[] args) {
+        
+    }
+    
+}

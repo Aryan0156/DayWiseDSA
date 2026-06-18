1fc2a473e59bbb847cc1625b3e078882a0071d48
Sahil Khan <arya89nkhan@gmail.com>
2026-06-18 11:34:38 +0530

    Majority element

---
+++ Day05/MajorityElement.java
@@ -0,0 +1,32 @@
+package Day05;
+
+import java.util.Scanner;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        int n = 12;
+        int count=1;
+        int max = 0;
+        int num = 0;
+        int [] arr = {1,1,1,1,1,2,2,2,3,3,3,4};
+        for(int i =1; i<n; i++){
+            if(arr[i] == arr[i-1]){
+                count++;
+                
+                if(count> max){
+                    max = count;
+                num = arr[i];}
+                
+            }
+            else{
+                count = 1;
+            }
+        }
+        System.out.println(num + " Max Count = "+max);
+        
+    
+sc.close();
+    }
+    
+}

9d5633ac65391148d4f576fe48d7d3b44808eb0b
Sahil Khan <arya89nkhan@gmail.com>
2026-06-21 22:13:29 +0530

    KthBit

---
+++ Day07/KthBit.java
@@ -0,0 +1,20 @@
+package Day07;
+
+import java.util.Scanner;
+
+public class KthBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("---------------------");
+        int k = sc.nextInt();
+        if((n & (1<<k) )!= 0){
+            System.out.println("Bit set");
+        }
+        else{
+            System.out.println("Not a bit set");
+        }
+        sc.close();
+    }
+    
+}

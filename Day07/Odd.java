5d5bed9c4d196a743a9c78958679d64d098af3af
Sahil Khan <arya89nkhan@gmail.com>
2026-06-24 20:48:50 +0530

    update

--- Day07/Odd.java
+++ Day07/Odd.java
@@ -8,7 +8,6 @@ public class Odd{
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         int[] arr = {2, 3, 2, 4, 2, 4, 3, 5, 1, 1};
-        int res = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int x : arr){
             map.put(x, map.getOrDefault(x,0)+1);
